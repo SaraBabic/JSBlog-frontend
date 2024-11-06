2d592bfe5c205581e9faf82fca9aa502d0c16050
Sara <sara_babic@intention.de>
2024-11-06 21:22:54 +0100

    New section in blog page

--- src/pages/BlogSinglePage.js
+++ src/pages/BlogSinglePage.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Moment from 'moment';
-import MainLayout from '../layout/MainLayout';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Moment from "moment";
+import MainLayout from "../layout/MainLayout";
 
 function BlogSinglePage() {
   const { slug } = useParams();
@@ -14,25 +14,46 @@ function BlogSinglePage() {
         setArticle(data);
       });
   }, [slug]);
-
+  console.log("sjdifsjf :::", article);
   return (
     <MainLayout>
-
       {!article ? (
         <p>Loading...</p>
       ) : (
-        <div className="container mt-4">
-          <h1>{article.title}</h1>
-          <p className="text-muted">
-            {Moment(article.createdAt).format('D MMM YYYY')}
-          </p>
-          <p>{article.markdown}</p>
-          {article.imagePath && (
-          <div className="form-group">
-            <img src={`/${article.imagePath}`} alt="Current Article" style={{ maxWidth: '200px', margin: '10px 0' }} />
+        <>
+          <div className="container mt-4">
+            <h1>{article.title}</h1>
+            <p className="text-muted">
+              {Moment(article.createdAt).format("D MMM YYYY")}
+            </p>
+            <p>{article.markdown}</p>
+            {article.imagePath && (
+              <div className="form-group">
+                <img
+                  src={`/${article.imagePath}`}
+                  alt="Current Article"
+                  style={{ maxWidth: "200px", margin: "10px 0" }}
+                />
+              </div>
+            )}
           </div>
-        )}
-        </div>
+          {article.sections.length > 0
+            ? article.sections.map((section, index) => {
+                if (section.type === "text") {
+                  return <p key={index}>{section.content}</p>;
+                } else if (section.type === "image") {
+                  return (
+                    <img
+                      key={index}
+                      src={section.content}
+                      alt={`${index + 1}`}
+                    />
+                  );
+                }
+                return null;
+              })
+            : null}
+        </>
       )}
     </MainLayout>
   );
